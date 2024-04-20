09193b4e506fdea24e6cb2ec8aa97ab1ecf49870
Divya Chaudhary <167012595+divyaaachaudhary@users.noreply.github.com>
2024-04-20 05:42:09 +0530

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,8 @@
+<script>
+  const chatbotIcon = document.querySelector('.ChatBot1');
+  chatbotIcon.addEventListener('click', function() {
+    <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+    <script src="https://mediafiles.botpress.cloud/40f8bebe-697c-43b5-b96f-34cb1b2d0912/webchat/config.js" defer></script>
+    WebChat.open();
+  });
+</script>
